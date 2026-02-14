8bb4750942a0e129410f4a6e34b3083e12743867
josefilhoest <josefilhoelineide2236@hotmail.com>
2026-02-14 19:23:22 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function generateNumber() {
+
+    const min = Math.ceil(document.getElementById("min").value);
+
+    const max = Math.floor(document.getElementById("max").value);
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    
+
+    console.log("O número sorteado foi: " + result);
+    document.getElementById("result").textContent = " O número sorteado foi: " + result;
+
+ 
+}
